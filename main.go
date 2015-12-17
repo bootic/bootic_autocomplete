4af15c3288dbfb86d054e8d1e22e7d14e3ef1a67
Ismael Celis <ismaelct@gmail.com>
2015-12-17 12:18:12 +0000

    First import: working ES search results.

---
+++ main.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"encoding/json"
+	"flag"
+	"github.com/belogik/goes"
+	"log"
+	"net/http"
+	"runtime"
+	"strings"
+)
+
+func main() {
+
+	maxProcs := runtime.NumCPU()
+	runtime.GOMAXPROCS(maxProcs)
+
+	var (
+		es_host string
+	)
+
+	flag.StringVar(&es_host, "eshost", "localhost:9200", "HTTP host:port for ElasticSearch server")
+	flag.Parse()
+
+	es_host_and_port := strings.Split(es_host, ":")
+
+	engine := goes.NewConnection(es_host_and_port[0], es_host_and_port[1])
+
+	http.HandleFunc("/search", func(w http.ResponseWriter, r *http.Request) {
+
+		q := r.URL.Query().Get("q")
+		// fmt.Fprintf(w, "Search: %q", q)
+		query := map[string]interface{}{
+			"query": map[string]interface{}{
+				"filtered": map[string]interface{}{
+					"query": map[string]interface{}{
+						"query_string": map[string]interface{}{
+							"query":            q,
+							"default_operator": "AND",
+						},
+					},
+				},
+			},
+		}
+
+		searchResults, err := engine.Search(query, []string{"products"}, []string{"product"})
+
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		json_data, err := json.Marshal(searchResults)
+
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		w.Header().Set("Content-Type", "application/json")
+		w.Write(json_data)
+	})
+
+	log.Fatal(http.ListenAndServe(":3000", nil))
+}
